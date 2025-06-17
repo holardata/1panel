c82d615d6105ad1609b331b0df25e58c61b2ed6c
liwenjie <lwj@holardata.com>
2025-06-17 11:11:48 +0800

    logo

--- frontend/src/layout/components/Sidebar/components/Logo.vue
+++ frontend/src/layout/components/Sidebar/components/Logo.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: liwenjie lwj@holardata.com
+ * @Date: 2025-06-16 18:19:16
+ * @LastEditors: liwenjie lwj@holardata.com
+ * @LastEditTime: 2025-06-17 11:10:41
+ * @FilePath: /1Panel/frontend/src/layout/components/Sidebar/components/Logo.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
     <div class="logo" style="cursor: pointer" @click="goHome">
         <template v-if="isCollapse">
@@ -8,7 +16,7 @@
                 alt="logo"
             />
             <!-- <MenuLogo v-else /> -->
-            <img :src="MenuLogo" v-else />
+            <img :src="MenuLogo" style="height: 30px; width: auto" v-else />
         </template>
         <template v-else>
             <img
@@ -17,7 +25,7 @@
                 style="cursor: pointer"
                 alt="logo"
             />
-            <PrimaryLogo v-else />
+            <PrimaryLogo style="width: 90%; height: auto" v-else />
             <!-- <img :src="PrimaryLogo" alt="" /> -->
         </template>
     </div>

2fe0023153b7deea17f4c5d48755d2271571fa43
zhuzhiwu <jessezhu@qq.com>
2025-06-19 17:47:42 +0800

    添加部署相关脚本

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# 1panel.service 和 start.sh 都放到 /opt/1panel/ 目录下
+# 部署说明: 
+# 1. 把 conf目录下的 app.yaml 放到 /opt/1panel/conf 目录下
+# 2. 二进制 1panel, 1panel.service 和 start.sh 都放到 /opt/1panel/ 目录下
 PROJ_NAME="1panel"
 
 TARGET_DIR="/lib/systemd/system"
 SERVICE_NAME="${PROJ_NAME}.service"
 SOURCE_PATH="/opt/1panel/${SERVICE_NAME}"
+SOURCE_BIN="/opt/1panel/${PROJ_NAME}"
 
 # 检查是否传入了stop参数
 if [ "$1" = "stop" ]; then
@@ -14,6 +17,17 @@ if [ "$1" = "stop" ]; then
     ps auxf | grep -v "grep" |grep $PROJ_NAME
     # ps auxf |grep java |grep $PROJ_NAME |grep -v grep | awk '{print $2}' |xargs kill -9
     exit 0
+fi
+
+if [ -f "$SOURCE_BIN" ]; then
+    mv "$SOURCE_BIN" /usr/local/bin/ && \
+    chmod +x /usr/local/bin/$PROJ_NAME
+    if [ $? -eq 0 ]; then
+        echo "Successfully moved and set executable permission for $PROJ_NAME"
+    else
+        echo "Failed to move or set permission for $PROJ_NAME"
+        exit 1
+    fi
 fi
 
 if [ ! -f "$TARGET_DIR/$SERVICE_NAME" ]; then
---
+++ run_build.sh
@@ -0,0 +1,3 @@
+make build_on_arm64
+md5sum build/1panel
+cp build/1panel ~/Desktop/

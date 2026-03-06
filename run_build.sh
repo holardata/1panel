a19d377887055ec588e9b5b3583a88a0748150db
zhuzhiwu <jessezhu@qq.com>
2026-03-06 21:13:56 +0800

    feat(build): 添加 tar 打包选项以排除特定文件

--- run_build.sh
+++ run_build.sh
@@ -3,7 +3,15 @@
 make build_on_arm64
 rm -rf ./deploy/1panel/1panel && cp build/1panel ./deploy/1panel/
 NOW=$(date +%Y%m%d_%H%M%S)
-cd ./deploy/ && tar -zcvf 1panel_${NOW}.tar.gz 1panel && md5sum 1panel_${NOW}.tar.gz && cp 1panel_${NOW}.tar.gz ~/Desktop/
+
+TAR_OPTIONS=(
+    --exclude=".DS_Store"
+    --exclude="._*"
+    --no-xattrs
+    --no-mac-metadata
+)
+
+cd ./deploy/ && tar "${TAR_OPTIONS[@]}" -zcvf 1panel_${NOW}.tar.gz 1panel && md5sum 1panel_${NOW}.tar.gz && cp 1panel_${NOW}.tar.gz ~/Desktop/
 # 将1panel_${NOW}.tar.gz 上传到 10.10.10.10 服务器的 /opt/1panel/ 目录下
 # tar -zxvf 1panel_${NOW}.tar.gz -C /opt/
 # 重启服务

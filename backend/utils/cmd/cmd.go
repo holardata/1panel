60fe2590b73fd4b0cf558888bf46d460bd711cea
zhuzhiwu <jessezhu@qq.com>
2026-03-14 22:25:11 +0800

    fix: 将脚本执行超时时间从10分钟延长至60分钟

--- backend/utils/cmd/cmd.go
+++ backend/utils/cmd/cmd.go
@@ -140,7 +140,7 @@ func ExecScript(scriptPath, workDir string) (string, error) {
 	go func() {
 		done <- cmd.Wait()
 	}()
-	after := time.After(10 * time.Minute)
+	after := time.After(60 * time.Minute)
 	select {
 	case <-after:
 		_ = cmd.Process.Kill()

f652ff57f50a9d24d4bd3c143f4574fd818e2345
zhuzhiwu <jessezhu@qq.com>
2026-04-23 16:05:32 +0800

    chore: 移除已构建的二进制文件并添加构建脚本

---
+++ run_build_amd64.sh
@@ -0,0 +1,21 @@
+# 1. 修改 Makefile 中的 GOOS=linux GOARCH=arm64
+# 2. 修改 cmd/server/cmd/version.go 版本号
+make build_backend_on_darwin
+rm -rf ./deploy/1panel/1panel && cp build/1panel ./deploy/1panel/
+NOW=$(date +%Y%m%d_%H%M%S)
+
+TAR_OPTIONS=(
+    --exclude=".DS_Store"
+    --exclude="._*"
+    --no-xattrs
+    --no-mac-metadata
+)
+
+cd ./deploy/ && tar "${TAR_OPTIONS[@]}" -zcvf 1panel_${NOW}.tar.gz 1panel
+# md5sum 1panel_${NOW}.tar.gz && cp 1panel_${NOW}.tar.gz ~/Desktop/
+md5sum 1panel/1panel && cp 1panel/1panel ~/Desktop/
+
+# 将1panel_${NOW}.tar.gz 上传到 10.10.10.10 服务器的 /opt/1panel/ 目录下
+# tar -zxvf 1panel_${NOW}.tar.gz -C /opt/
+# 重启服务
+# systemctl restart 1panel
